5bf580eae45b23f2dda250e09a46f0151d14dd09
Dan Strong <danielstrong@mac.com>
2023-08-29 18:58:58 +0000

    initial setup for useRef

---
+++ pages/imageChangeOnMouseover.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageChangeOnMouseOver = () => {
+    return (
+        <div>
+            <img src='' alt=''/>
+            <img src='' alt=''/>
+        </div>
+    );
+
+}
+
+export default ImageChangeOnMouseOver;
